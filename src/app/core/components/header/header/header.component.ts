23969b2d36c8eb20cfe43c39e162bf80af889d04
Runeci <kostenokd@mail.ru>
2022-04-29 13:37:51 +0300

    feat: add header change

--- src/app/core/components/header/header/header.component.ts
+++ src/app/core/components/header/header/header.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  currentRoute: string;
+
+  constructor(private router: Router) {
+    this.currentRoute = '';
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
+}
