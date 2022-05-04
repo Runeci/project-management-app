bf703e14746a9deae6f0d33e3cd00f308a18b96a
Runeci <kostenokd@mail.ru>
2022-05-04 13:59:32 +0300

    feat: add ability to drag n drop tasks

--- src/app/boards/components/board-column/board-column.component.ts
+++ src/app/boards/components/board-column/board-column.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Column } from '@shared/models/columns.interfaces';
+import { TaskI } from '@shared/models/tasks.interfaces';
+import { TaskApiService } from '@boards/services/task-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Board } from '@shared/models/boards.interfaces';
 
 @Component({
   selector: 'app-board-column',
@@ -10,17 +14,63 @@ import { Column } from '@shared/models/columns.interfaces';
 export class BoardColumnComponent implements OnInit {
   @Input() column!: Column;
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  public tasksArr: TaskI[] = [];
 
-  constructor() {
+  private boardId: Board['id'];
+
+  constructor(private tasksApiService: TaskApiService,
+              private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+    this.boardId = this.activatedRoute.snapshot.params['id'];
+
+    this.tasksApiService.getTasks(this.boardId, this.column.id)
+      .subscribe((res) => {
+        this.tasksArr =
+          res.sort((prev, next) => prev.order - next.order);
+      });
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  private updateTasksOrder(tasksArr: TaskI[], columnId: Column['id']) {
+    tasksArr.forEach((task, index) => {
+      this.tasksApiService.updateTask(
+        this.boardId,
+        columnId,
+        task.id,
+        {
+          title: task.title,
+          order: index + 1,
+          description: task.description,
+          userId: task.userId,
+          boardId: task.boardId,
+          columnId: columnId,
+        }
+      ).subscribe();
+    });
+  }
+
+  public deleteTask(columnId: Column['id'], taskId: TaskI['id']) {
+    this.tasksApiService
+      .deleteTask(this.boardId, columnId, taskId).subscribe();
+  }
+
+  public addTask() {
+    this.tasksApiService.createTask(this.boardId, this.column.id,
+      {
+        title: Math.random().toString(),
+        order: this.tasksArr.length + 1,
+        description: 'hi',
+        userId: '55e8067c-0333-46a8-973d-b616a97aa905',
+      }
+    ).subscribe();
+  }
+
+  public drop(event: CdkDragDrop<TaskI[]>) {
+    const draggedTask = event.item.data;
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      this.updateTasksOrder(event.container.data, this.column.id);
     } else {
       transferArrayItem(
         event.previousContainer.data,
@@ -28,6 +78,24 @@ export class BoardColumnComponent implements OnInit {
         event.previousIndex,
         event.currentIndex,
       );
+
+      this.tasksApiService.updateTask(this.boardId, draggedTask.columnId, draggedTask.id,
+        {
+          title: draggedTask.title,
+          order: event.container.data.indexOf(draggedTask) + 1,
+          userId: draggedTask.userId,
+          description: draggedTask.description,
+          boardId: this.boardId,
+          columnId: this.column.id,
+        }).subscribe(
+        () => {
+          this.updateTasksOrder(event.container.data, this.column.id);
+          console.log(event.previousContainer.data);
+        }
+      );
+
+      this.updateTasksOrder(event.previousContainer.data, draggedTask.columnId);
+      draggedTask.columnId = this.column.id;
     }
   }
 }
--- src/app/boards/components/column-task/column-task.component.ts
+++ src/app/boards/components/column-task/column-task.component.ts
@@ -1,7 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {
- animate, state, style, transition, trigger,
+  animate, state, style, transition, trigger,
 } from '@angular/animations';
+import { TaskI } from '@shared/models/tasks.interfaces';
+import { TaskApiService } from '@boards/services/task-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Board } from '@shared/models/boards.interfaces';
+import { Column } from '@shared/models/columns.interfaces';
 
 @Component({
   selector: 'app-column-task',
@@ -19,8 +24,20 @@ import {
     ]),
   ],
 })
-export class ColumnTaskComponent {
-  @Input() task: any;
+export class ColumnTaskComponent implements OnInit {
+  @Input() task!: TaskI;
+
+  @Input() column!: Column;
+
+  private boardId: Board['id'];
+
+  constructor(private tasksApiService: TaskApiService,
+              private activatedRoute: ActivatedRoute) {
+  }
+
+  public ngOnInit() {
+    this.boardId = this.activatedRoute.snapshot.params['id'];
+  }
 
   public animationStatus: string = 'start';
 
@@ -30,5 +47,10 @@ export class ColumnTaskComponent {
 
   public stopAnimation(): void {
     this.animationStatus = 'start';
+  }
+
+  public deleteTask(taskId: TaskI['id']) {
+    this.tasksApiService
+      .deleteTask(this.boardId, this.column.id, taskId).subscribe();
   }
 }
